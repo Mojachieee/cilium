4ab8bee413e41430d7c64d5fee7e10d249904567
Chris Tarazi <chris@isovalent.com>
2024-04-08 21:26:27 -0700

    squash: slim apiextensions v1
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/k8s/slim/k8s/apis/apiextensions/v1/doc.go
+++ pkg/k8s/slim/k8s/apis/apiextensions/v1/doc.go
@@ -5,10 +5,10 @@
 
 // +k8s:deepcopy-gen=package
 // +k8s:protobuf-gen=package
-// +k8s:conversion-gen=false
+// +k8s:conversion-gen=k8s.io/apiextensions-apiserver/pkg/apis/apiextensions
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
 // +groupName=apiextensions.k8s.io
 
 // Package v1 is the v1 version of the API.
-package v1
+package v1 // import "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
