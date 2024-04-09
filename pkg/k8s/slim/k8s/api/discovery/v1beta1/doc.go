72b3facf430ae4e0a1f0e20e68202aac8573ad0e
Chris Tarazi <chris@isovalent.com>
2024-04-08 21:11:54 -0700

    squash: slim discovery v1beta1
    Signed-off-by: Chris Tarazi <chris@isovalent.com>

--- pkg/k8s/slim/k8s/api/discovery/v1beta1/doc.go
+++ pkg/k8s/slim/k8s/api/discovery/v1beta1/doc.go
@@ -5,8 +5,6 @@
 // +k8s:protobuf-gen=package
 // +k8s:openapi-gen=true
 // +k8s:prerelease-lifecycle-gen=true
-// +deepequal-gen=package
 // +groupName=discovery.k8s.io
 
-// Package v1beta1 contains slimmer versions of k8s discovery types.
-package v1beta1
+package v1beta1 // import "k8s.io/api/discovery/v1beta1"
